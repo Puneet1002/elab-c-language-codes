853bc0619fe4ca925cc235a6551126512c56a3d8
Puneet1002 <86978941+Puneet1002@users.noreply.github.com>
2023-03-27 22:44:43 +0530

    Create on one beautiful Sunday.c

---
+++ on one beautiful Sunday.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float num1,num2;
+    double resnum1,resnum2;
+    scanf("%f\n%f\n",&num1,&num2);
+    resnum1=(double)num1;
+    resnum2=(double)num2;
+    printf("%f\n%f\n",resnum1,resnum2);
+
+	return 0;
+}
